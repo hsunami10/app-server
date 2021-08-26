d869eca6d2580f192b26aaf38ffb72e878ebae09
Michael Hsu <michael100.hsu@gmail.com>
2021-08-26 17:07:58 -0500

    update eslint

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 /* @flow strict */
 
 const x = (z: number): boolean => {
-  const owqier = "hello";
-  var asd = "no";
   let y = 20;
   return y === z;
 };
-
 console.log("hello");
 
 export default x;
