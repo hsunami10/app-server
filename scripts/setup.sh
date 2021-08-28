dc73036298092f54989ed70d6973644d08cbf07c
Michael Hsu <michael100.hsu@gmail.com>
2021-08-28 16:35:08 -0500

    fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,6 @@ if [ ! -d "${gitroot}" ]; then
 fi
 
 sh $gitroot/scripts/pre-commit/install_pre_commit_hook.sh
-# sh $gitroot/scripts/psql/install.sh
+sh $gitroot/scripts/psql/install.sh
 sh $gitroot/scripts/watchman/setup.sh
 
